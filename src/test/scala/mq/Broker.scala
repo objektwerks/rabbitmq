323c0212922fffbad634fd6df1dfe06d7c2bc918
objektwerks <objektwerks@gmail.com>
2016-05-11 07:51:56 -0500

    refactored broker and test

--- src/test/scala/mq/Broker.scala
+++ src/test/scala/mq/Broker.scala
@@ -2,15 +2,27 @@ package mq
 
 import akka.actor.{Actor, Props}
 
-class Broker(requestQueue: QueueConnector, responseQueue: QueueConnector) extends Actor {
+class Broker extends Actor {
   val worker = context.actorOf(Props[Worker])
+  val requestQueue = new QueueConnector("request.queue.conf")
+  val responseQueue = new QueueConnector("response.queue.conf")
+  requestQueue.push("test.request")
 
   override def receive: Receive = {
     case WorkRequest =>
-      val request = requestQueue.pull.get
-      worker ! Request(id = request.getEnvelope.getDeliveryTag, message = new String(request.getBody))
+      requestQueue.pull match {
+        case Some(item) =>
+          worker ! Request(id = item.getEnvelope.getDeliveryTag, message = new String(item.getBody))
+        case None =>
+      }
     case response: Response =>
       responseQueue.push(response.message)
       requestQueue.ack(response.id)
+  }
+
+  @scala.throws[Exception](classOf[Exception])
+  override def postStop: Unit = {
+    requestQueue.close()
+    responseQueue.close()
   }
 }
--- src/test/scala/mq/BrokerTest.scala
+++ src/test/scala/mq/BrokerTest.scala
@@ -10,21 +10,15 @@ import scala.concurrent.duration._
 
 class BrokerTest extends FunSuite  with BeforeAndAfterAll {
   implicit val timeout = Timeout(1 second)
-  val requestQueue = new QueueConnector("request.queue.conf")
-  val responseQueue = new QueueConnector("response.queue.conf")
   val system: ActorSystem = ActorSystem.create("queue", ConfigFactory.load("test.conf"))
-  val broker = system.actorOf(Props(new Broker(requestQueue, responseQueue)))
+  val broker = system.actorOf(Props[Broker])
 
   override protected def afterAll(): Unit = {
     Await.result(system.terminate(), 1 second)
-    requestQueue.close()
-    responseQueue.close()
   }
 
   test("broker") {
-    requestQueue.push("test.request")
+    Thread.sleep(1000)
     broker ! WorkRequest
-    assert(requestQueue.pull.isEmpty)
-    assert(responseQueue.pull.nonEmpty)
   }
 }
