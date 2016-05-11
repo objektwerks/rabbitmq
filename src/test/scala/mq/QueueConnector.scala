dc01d290e9eed660ca6bc5686ddcfbd720ec9258
objektwerks <objektwerks@gmail.com>
2016-05-11 07:08:32 -0500

    refactored queue connector

--- src/test/scala/mq/QueueConnector.scala
+++ src/test/scala/mq/QueueConnector.scala
@@ -5,19 +5,20 @@ import com.typesafe.config.ConfigFactory
 
 class QueueConnector(configName: String) {
   val config = ConfigFactory.load(configName)
+  val exchange = config.getString("amqp.channel.exchange")
+  val exchangeType = config.getString("amqp.channel.exchangeType")
+  val routingKey = config.getString("amqp.channel.routingKey")
+  val queue = config.getString("amqp.channel.queue")
+  val durable = config.getBoolean("amqp.channel.durable")
+  val autoAck = config.getBoolean("amqp.channel.autoAck")
 
   val factory = new ConnectionFactory()
   factory.setUri(config.getString("amqp.url"))
   val connection = factory.newConnection()
 
   val channel = connection.createChannel
-  val exchange = config.getString("amqp.channel.exchange")
-  val exchangeType = config.getString("amqp.channel.exchangeType")
-  val routingKey = config.getString("amqp.channel.routingKey")
-  val queue = config.getString("amqp.channel.queue")
-  val autoAck = config.getBoolean("amqp.channel.autoAck")
   channel.exchangeDeclare(exchange, exchangeType, true)
-  channel.queueDeclare(queue, true, false, false, null)
+  channel.queueDeclare(queue, durable, false, false, null)
   channel.queueBind(queue, exchange, routingKey)
 
   def close(): Unit = connection.close()
@@ -26,7 +27,13 @@ class QueueConnector(configName: String) {
 
   def pull: Option[GetResponse] = Option(channel.basicGet(queue, autoAck))
 
-  def ack(deliveryTag: Long): Unit = channel.basicAck(deliveryTag, false)
+  def ack(deliveryTag: Long): this.type = {
+    channel.basicAck(deliveryTag, false)
+    this
+  }
 
-  def nack(deliveryTag: Long): Unit = channel.basicNack(deliveryTag, false, true)
+  def nack(deliveryTag: Long): this.type = {
+    channel.basicNack(deliveryTag, false, true)
+    this
+  }
 }
