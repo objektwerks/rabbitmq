17488d74bae597a76c74422a61b8eb9e97258db4
objektwerks <objektwerks@gmail.com>
2016-06-30 06:44:24 -0500

    parameterized test helper methods for number of messages to pull and push to queue

--- src/test/scala/mq/QueueConnectorTest.scala
+++ src/test/scala/mq/QueueConnectorTest.scala
@@ -20,37 +20,37 @@ class QueueConnectorTest extends FunSuite with BeforeAndAfterAll {
   }
 
   test("amqp") {
-    pushMessagesToRequestQueue()
-    pullMessagesFromRequestQueue()
+    pushMessagesToRequestQueue(10)
+    pullMessagesFromRequestQueue(10)
     Thread.sleep(3000)
   }
 
   test("broker") {
-    pushMessagesToRequestQueue()
+    pushMessagesToRequestQueue(10)
     broker ! WorkRequest
     Thread.sleep(3000)
   }
 
-  private def pushMessagesToRequestQueue(): Unit = {
+  private def pushMessagesToRequestQueue(numberOfMessages: Int): Unit = {
     val requestQueue = new QueueConnector("test.request.queue.conf")
     val counter = new AtomicInteger()
     val confirmed = new AtomicInteger()
-    for (i <- 1 to 100) {
+    for (i <- 1 to numberOfMessages) {
       val message = s"test.request: ${counter.incrementAndGet}"
       val isComfirmed = requestQueue.push(message)
       if (isComfirmed) confirmed.incrementAndGet
     }
     requestQueue.close()
-    assert(confirmed.intValue == 100)
+    assert(confirmed.intValue == numberOfMessages)
   }
 
-  private def pullMessagesFromRequestQueue(): Unit = {
+  private def pullMessagesFromRequestQueue(numberOfMessages: Int): Unit = {
     val requestQueue = new QueueConnector("test.request.queue.conf")
     val pulled = new AtomicInteger()
-    for (i <- 1 to 100) {
+    for (i <- 1 to numberOfMessages) {
       if(requestQueue.pull.nonEmpty) pulled.incrementAndGet
     }
     requestQueue.close()
-    assert(pulled.intValue == 100)
+    assert(pulled.intValue == numberOfMessages)
   }
 }
